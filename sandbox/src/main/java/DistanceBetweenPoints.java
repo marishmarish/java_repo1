893471f958989eb8788438dd2c04449dbf29428c
marishmarish <marishmarish@gmail.com>
2019-05-26 16:13:20 +0300

    Задание 2: Distance between 2 points

---
+++ sandbox/src/main/java/DistanceBetweenPoints.java
@@ -0,0 +1,17 @@
+public class DistanceBetweenPoints {
+    public static void main(String[] args) {
+        Point pointFirst = new Point(5, 4);
+        Point pointSecond = new Point(1, 1);
+        System.out.println("Distance between 2 points: " + distance(pointFirst, pointSecond));
+        double distance = pointSecond.distance(pointFirst);
+        System.out.println("Distance between 2 points: " + distance);
+
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double xResult = p1.x - p2.x;
+        double yResult = p1.y - p2.y;
+        double sumXY = xResult * xResult + yResult * yResult;
+        return Math.sqrt(sumXY);
+    }
+}
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    double x;
+    double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point p1) {
+        double xResult = p1.x - this.x;
+        double yResult = p1.y - this.y;
+        double sumXY = xResult * xResult + yResult * yResult;
+        return Math.sqrt(sumXY);
+    }
+}
