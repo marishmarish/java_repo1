c10c2aedcaad4efb3c6ed32c3cde6acd7c9a1698
marishmarish <marishmarish@gmail.com>
2019-06-02 15:23:07 +0300

    Задание 4. Промежуточные результаты ContactCreationTests
    - Записан в рекордере тест для создания нового контакта;
    - Тест переведен на язык Java и перенесен в среду разработки;
    - Удалены все неисползуемые методы, переменные и поля;
    - переимование переменной webdriver= wb.

---
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,59 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+public class ContactCreationTests {
+  private WebDriver wd;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testContactCreation() {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.id("LoginForm")).click();
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys("First");
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys("Name");
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys("1234567890");
+    wd.findElement(By.name("theform")).click();
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys("first@name.com");
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    wd.findElement(By.linkText("home page")).click();
+    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() {
+    wd.quit();
+  }
+
+}
