f616d0ff0c23617148a076aaa915f5d88e6fc9c4
marishmarish <marishmarish@gmail.com>
2019-07-19 23:33:28 +0300

    Лекция 9.3. SOAP: взаимодействие с MantisBT
    созаны тесты для выведенния названия проектов на консоль и для создания багаЖ
    testGetProjects()
    testCreateIssue()

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -22,6 +22,8 @@ public class ApplicationManager {
     private JamesHelper jamesHelper;
     private LoginHelper loginHelper;
     private UserManagerHelper userManagerHelper;
+    private SoapHelper soapHelper;
+
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -103,5 +105,12 @@ public class ApplicationManager {
             jamesHelper = new JamesHelper(this);
         }
         return jamesHelper;
+    }
+
+    public SoapHelper soap() {
+        if (soapHelper == null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
